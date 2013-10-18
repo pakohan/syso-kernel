67279b6e2a67464e3c80c56f057f2fcfb39b2643
Patrick Kohan <patrick.kohan@googlemail.com>
2013-10-18 13:17:13 +0200

    V1 0.5 alpha

--- V1/build.sh
+++ V1/build.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-rm -rf rootfs
-rm init
-rm ramdisk.img
-#rm -rf linux-3.10.16
-#tar xJf linux-3.10.16.tar.xz
+gcc -static -o ../linux-3.10.16/usr/init init.c -m32
 cd ../linux-3.10.16
-make mrproper ARCH=i386
-make allnoconfig ARCH=i386
-#make i386_defconfig ARCH=i386
-make nconfig ARCH=i386
+#make mrproper ARCH=i386
+#make allnoconfig ARCH=i386
+cp ../V1/.config .
 make -j3 ARCH=i386
 cd ../V1
-gcc -static -o init init.c -m32
-dd if=/dev/zero of=ramdisk.img bs=1M count=8
-mkfs.ext2 ramdisk.img
-mkdir rootfs
-sudo mount -o loop ramdisk.img rootfs
-sudo cp init rootfs
-sudo mkdir rootfs/dev
-sudo mknod rootfs/dev/console c 5 1
-sudo mknod rootfs/dev/ram b 1 0
-sudo umount rootfs
-qemu -kernel linux-3.10.16/arch/x86/boot/bzImage  -initrd ramdisk.img  -append "root=/dev/ram init=/init"
+qemu -kernel ../linux-3.10.16/arch/x86/boot/bzImage -append "root=/dev/ram init=/init"
--- V1/init.c
+++ V1/init.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/sysinfo.h>
+#include <stdlib.h>
+
 int  main(void)
 {
-        while(1){
-        printf("hello,world\n");
-        usleep(1000000);
+    struct sysinfo *info;
+    int code;
+    info = (struct sysinfo*) malloc(sizeof(struct sysinfo));
+    if (info == NULL) {
+        printf("malloc failed");
+    } else {
+        code = sysinfo(info);
+        if (code != 0) {
+            printf("sysinfo failed");
+        } else {
+            printf("Hello User world\nUptime: %ld\nTotal RAM: %lu\nFree RAM: %lu\nProcess count: %d\nPage size: %d\n", info->uptime, info->totalram, info->freeram, info->procs, info->mem_unit);
         }
-        return 0;
+    }
+
+
+    while(1){
+        printf("hello, User Tobi\n");
+        usleep(1000000);
+    }
+    return 0;
 }
+
+
+
+
+
