3fb30e620fd0f19a2fc7caadd5cbc72804d6f976
Patrick Kohan <patrick.kohan@googlemail.com>
2013-10-19 17:53:17 +0200

    update

--- V1/build.sh
+++ V1/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 rm -rf ../linux-3.10.16/usr/initramfs
+rm ../linux-3.10.16/usr/.initramfs_data.cpio.d
+
 mkdir ../linux-3.10.16/usr/initramfs
 mkdir ../linux-3.10.16/usr/initramfs/bin
 mkdir ../linux-3.10.16/usr/initramfs/etc
 mkdir ../linux-3.10.16/usr/initramfs/dev
 mkdir ../linux-3.10.16/usr/initramfs/proc
 mkdir ../linux-3.10.16/usr/initramfs/usr
-mkdir ../linux-3.10.16/usr/initramfs/usr/webmin
 
+cp -R ../webmin-1.660 ../linux-3.10.16/usr/initramfs/usr/webmin/
 cp inittab ../linux-3.10.16/usr/initramfs/etc
 
 cd ../perl-5.8.9
 make CC=../buildroot-2013.08.1/output/host/usr/bin/i386-buildroot-linux-uclibc-gcc ARCH=i386 CROSS_COMPILE=i386-buildroot-linux-uclibc- -f Makefile.micro
 cp microperl ../linux-3.10.16/usr/initramfs/bin
-
-cp -R ../webmin-1.660/ ../linux-3.10.16/usr/initramfs/usr/webmin
 
 cd ../busybox-1.21.1/
 cp ../V1/config-busybox .config
